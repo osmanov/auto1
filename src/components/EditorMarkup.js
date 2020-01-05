8081ee9491547d40961ca81621d05f300139f969
osmanov <rhenium.osmium@gmail.com>
2020-01-06 01:38:58 +0300

    feature:edotor with newPost tests

--- src/components/EditorMarkup.js
+++ src/components/EditorMarkup.js
@@ -1,21 +1,31 @@
 import React from 'react'
-
+import { savePost } from '../api'
 function Editor() {
   const [isDisabled, setIsDisabled] = React.useState(false)
   function handleSubmit(e) {
     e.preventDefault()
     setIsDisabled(true)
+
+    const { title, content, tags } = e.target.elements
+    const newPost = {
+      title: title.value,
+      content: content.value,
+      tags: tags.value.split()
+    }
+    savePost(newPost).then(() => {
+      setIsDisabled(false)
+    })
   }
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="title-input">Title</label>
-      <input id="title-input" />
+      <input id="title-input" name="title" />
 
       <label htmlFor="content-input">Content</label>
-      <textarea id="content-input" />
+      <textarea id="content-input" name="content" />
 
       <label htmlFor="tags-input">Tags</label>
-      <input id="tags-input" />
+      <input id="tags-input" name="tags" />
 
       <button type="submit" disabled={isDisabled}>
         Submit
--- src/components/__tests__/EditorMarkup.js
+++ src/components/__tests__/EditorMarkup.js
@@ -2,14 +2,33 @@ import React from 'react'
 import { Editor } from '../EditorMarkup'
 import user from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, wait } from '@testing-library/react'
+import { savePost as mockSavePost } from '../../api'
+jest.mock('../../api')
 
-test('render form with title,sbm btn,content,tags', () => {
+afterEach(() => {
+  jest.clearAllMocks()
+})
+test('render form with title,sbm btn,content,tags', async () => {
+  mockSavePost.mockResolvedValueOnce({ success: true })
   const { getByLabelText, getByText, debug } = render(<Editor />)
-  getByLabelText(/title/i)
-  getByLabelText(/tags/i)
-  getByLabelText(/content/i)
+  const title = 'Test Title'
+  const tags = 'tag1, tag2'
+  const content = 'Test content'
+  user.type(getByLabelText(/title/i), title)
+  user.type(getByLabelText(/content/i), content)
+  user.type(getByLabelText(/tags/i), tags)
+
   const submitBtn = getByText(/submit/i)
   fireEvent.click(submitBtn)
   expect(submitBtn).toBeDisabled()
+  expect(mockSavePost).toHaveBeenCalledWith({
+    title,
+    tags: tags.split(),
+    content
+  })
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
+  await wait(() => {
+    expect(submitBtn).toBeEnabled()
+  })
 })
