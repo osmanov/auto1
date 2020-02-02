9c0bf1b8e2e7842c70282639226bcb74f24e8f48
osmanov <rhenium.osmium@gmail.com>
2020-02-02 23:54:43 +0300

    feature:check oinks behavior

---
+++ src/components/__tests__/Crumbs.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { Main } from '../Crumbs'
+
+test('we can navigate', () => {
+  const history = createMemoryHistory({
+    initialEntries: ['/']
+  })
+  const { getByText, getByRole } = render(
+    <Router history={history}>
+      <Main />
+    </Router>
+  )
+  expect(getByRole('heading')).toHaveTextContent(/home/i)
+  const about = getByText(/about/i)
+  fireEvent.click(about)
+  expect(getByRole('heading')).toHaveTextContent(/about/i)
+})
